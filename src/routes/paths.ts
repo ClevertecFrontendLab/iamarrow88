ea37f4c9458aa9c82641fd3a42995e79bbb5decd
Nastya Pisanka <bonjozzy@gmail.com>
2024-02-25 20:27:34 +0300

    feat: add router

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,17 +1,24 @@
 export enum Paths {
-    MainPage = 'main/', // main page
-    Auth = 'auth/', //таб "Вход". if (loggedIn) redirect to /main
-    Registration = 'registration', // 'Registration' tab. if (!loggedIn)
+    Root = '/',
+    MainPage = '/main', // main page
+
+    Auth = '/auth', //таб "Вход". if (loggedIn) redirect to /main
+    //подразделы:
     ConfirmEmail = 'confirm-email', //ввод кода для восстановления пароля
     ChangePassword = 'change-password', //ввод нового пароля
-    CheckEmail = 'check-email', // ссылка "Забыли пароль?", только на табе "Вход" и после ввода e-mail
+
+    //Результаты
     Result = 'result/', //прямой переход не возможен на этот роут и его дочерние роуты
     LoginError = 'error-login', // "Вход" -> ошибка -> “Повторить” -> редирект на страницу /auth
     RegistrationSuccess = 'success',
     RegistrationErrorUserExist = 'error-user-exist', // status 409 -> “Назад к регистрации” -> /auth/registration
-    RegistrationError = 'error', //error !== 409 -> “Повторить” -> /auth/registration + new Request
+    RegistrationOtherErrors = 'error', //error !== 409 -> “Повторить” -> /auth/registration + new Request
     RecoveryNoExist = 'error-check-email-no-exist', // status 404 и message ‘Email не найден’ -> “Попробовать снова” -> /auth
     ErrorLogin = 'error-check-email', //с любым другим status, а также status 404 но без message ‘Email не найден' -> “Назад” -> /auth + new Request
     ChangePasswordError = 'error-change-password', //ошибка после ввода нового пароля. любой status ->  “Повторить” -> /change-password + new Request
     ChangePasswordSuccess = 'success-change-password', //пароль изменен
+
+    Registration = 'registration', // 'Registration' tab. if (!loggedIn)
+
+
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,32 +2,64 @@ import { Route, Routes } from 'react-router-dom';
 import {Paths} from "./paths.ts";
 import {MainPage} from "@pages/main-page";
 import EntryPage from "@pages/entry-page/entry-page.tsx";
+import Registration from "@components/blocks/auth/registration/registration.tsx";
 import Login from "@components/blocks/auth/login/login.tsx";
-import Registration from "@components/blocks/auth/registration/Registration.tsx";
-
+import Recovery from "@components/blocks/auth/recovery/recovery.tsx";
+import ConfirmEmail from "@components/blocks/auth/messages/confirmEmail/confirmEmail.tsx";
+import EntryWindow from "@components/blocks/auth/entry-window/entry-window.tsx";
+import ErrorWindow from "@components/blocks/auth/messages/error-window/error-window.tsx";
+import resultMessages from "@constants/content/resultMessages/resultMessages.ts";
 
 export const routes = (
     <Routes>
-        <Route path='/' element={<MainPage />}>
+        <Route path={Paths.Root}>
             <Route path={Paths.MainPage} element={<MainPage/>} />
             <Route path={Paths.Auth} element={<EntryPage/>}>
-                <Route index={true} element={<Login />} />
-                <Route path='registration' element={<Registration />} />
-                <Route path={Paths.ConfirmEmail} element={<Registration />} />
-                <Route path={Paths.ChangePassword} element={<Registration />} />
-                <Route path={Paths.CheckEmail} element={<Registration />} />
+                <Route index={true} element={<EntryWindow children={<Login />} /> } />
+                <Route path={Paths.Registration} element={<EntryWindow children={<Registration />} />} />
+                <Route path={Paths.ConfirmEmail} element={<EntryWindow children={<ConfirmEmail />} />} />
+                <Route path={Paths.ChangePassword} element={<EntryWindow children={<Recovery />} />} />
             </Route>
+
             <Route path={Paths.Result} element={<EntryPage />}>
-                <Route index={true} element={<Login />} />
-                <Route path={Paths.LoginError} element={<Registration />} />
-                <Route path={Paths.RegistrationSuccess} element={<Registration />} />
-                <Route path={Paths.RegistrationErrorUserExist} element={<Registration />} />
-                <Route path={Paths.RegistrationError} element={<Registration />} />
-                <Route path={Paths.RecoveryNoExist} element={<Registration />} />
-                <Route path={Paths.ErrorLogin} element={<Registration />} />
-                <Route path={Paths.ChangePasswordError} element={<Registration />} />
-                <Route path={Paths.ChangePasswordSuccess} element={<Registration />} />
+                <Route path={Paths.LoginError} element={<ErrorWindow icon={resultMessages.entryAnyError.icon}
+                                                                     title={resultMessages.entryAnyError.title}
+                                                                     buttonName={resultMessages.entryAnyError.buttonName}
+                                                                     description={resultMessages.entryAnyError.description} />} />
+
+
+                <Route path={Paths.RegistrationSuccess} element={<ErrorWindow icon={resultMessages.RegistrationSuccess.icon}
+                                                                              title={resultMessages.RegistrationSuccess.title}
+                                                                              buttonName={resultMessages.RegistrationSuccess.buttonName}
+                                                                              description={resultMessages.RegistrationSuccess.description} />} />
+                <Route path={Paths.RegistrationErrorUserExist} element={<ErrorWindow icon={resultMessages.registrationError409.icon}
+                                                                                     title={resultMessages.registrationError409.title}
+                                                                                     buttonName={resultMessages.registrationError409.buttonName}
+                                                                                     description={resultMessages.registrationError409.description} />} />
+                <Route path={Paths.RegistrationOtherErrors} element={<ErrorWindow icon={resultMessages.registrationOtherErrors.icon}
+                                                                                  title={resultMessages.registrationOtherErrors.title}
+                                                                                  buttonName={resultMessages.registrationOtherErrors.buttonName}
+                                                                                  description={resultMessages.registrationOtherErrors.description} />} />
+                <Route path={Paths.RecoveryNoExist} element={<ErrorWindow icon={resultMessages.recoveryError404Message.icon}
+                                                                          title={resultMessages.recoveryError404Message.title}
+                                                                          buttonName={resultMessages.recoveryError404Message.buttonName}
+                                                                          description={resultMessages.recoveryError404Message.description} />} />
+                <Route path={Paths.ErrorLogin} element={<ErrorWindow icon={resultMessages.recoveryOtherErrorsOrMessages.icon}
+                                                                     title={resultMessages.recoveryOtherErrorsOrMessages.title}
+                                                                     buttonName={resultMessages.recoveryOtherErrorsOrMessages.buttonName}
+                                                                     description={resultMessages.recoveryOtherErrorsOrMessages.description} />} />
+                <Route path={Paths.ChangePasswordError} element={<ErrorWindow icon={resultMessages.recoveryServerError.icon}
+                                                                              title={resultMessages.recoveryServerError.title}
+                                                                              buttonName={resultMessages.recoveryServerError.buttonName}
+                                                                              description={resultMessages.recoveryServerError.description} />} />
+                <Route path={Paths.ChangePasswordSuccess} element={<ErrorWindow icon={resultMessages.recoverySuccess.icon}
+                                                                                title={resultMessages.recoverySuccess.title}
+                                                                                buttonName={resultMessages.recoverySuccess.buttonName}
+                                                                                description={resultMessages.recoverySuccess.description} />} />
+
             </Route>
         </Route>
+
+
     </Routes>
 )
