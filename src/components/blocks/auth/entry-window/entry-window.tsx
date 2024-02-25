da85c22d7cf7614bebdffc4d7bc9bb1d3c8acf09
Nastya Pisanka <bonjozzy@gmail.com>
2024-02-25 20:23:19 +0300

    feat: create Entry-window

---
+++ src/components/blocks/auth/entry-window/entry-window.tsx
@@ -0,0 +1,49 @@
+import Logo from "@components/blocks/common/logo/logo.tsx";
+import {Tabs} from "antd";
+import './entry-window.css';
+import {ReactElement} from "react";
+import { useNavigate} from "react-router-dom";
+
+
+const EntryWindow = ({children}: {children: ReactElement}) => {
+    const navigate = useNavigate();
+    const onChange = (key: string): void => {
+        navigate(key, {replace: true})
+    }
+    const items = [
+        {
+            label: `Вход`,
+            key: '/auth',
+        },
+        {
+            label: `Регистрация`,
+            key: 'registration',
+        },
+    ]
+
+    return (
+        <div className="entry-window">
+            <Logo isCollapsed={false} className='entry-logo' />
+
+            <div className="content-wrapper">
+                <Tabs
+                    defaultActiveKey={'/'}
+                    size="middle"
+                    items={items}
+                    onChange={onChange}
+                    tabBarStyle={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                        marginBottom: '24px',
+                        fontFamily: "Inter, sans-sarif"
+                    }}
+                />
+                {children}
+            </div>
+
+
+        </div>
+    );
+};
+
+export default EntryWindow;
